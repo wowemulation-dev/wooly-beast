c6939905405a90b017e76c2ea4065f1c59704b18
Ovahlord <18347559+Ovahlord@users.noreply.github.com>
2024-04-26 01:37:27 +0200

    Core/Player: moved the periodic group update mechanic into heartbeat handling
    (cherry-picked from commit 80d72a2)

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -945,6 +945,8 @@ class TC_GAME_API Player : public Unit, public GridObject<Player>
 
         void Update(uint32 time) override;
 
+        void Heartbeat() override;
+
         static bool BuildEnumData(PreparedQueryResult result, WorldPacket* data);
 
         bool IsImmunedToSpellEffect(SpellInfo const* spellInfo, SpellEffectInfo const& spellEffectInfo, WorldObject const* caster, bool requireImmunityPurgesEffectAttribute = false) const override;
@@ -2495,8 +2497,6 @@ class TC_GAME_API Player : public Unit, public GridObject<Player>
         uint8 m_fishingSteps;
 
         bool m_needsZoneUpdate;
-
-        TimeTracker m_groupUpdateTimer;
 
     private:
         // internal common parts for CanStore/StoreItem functions
